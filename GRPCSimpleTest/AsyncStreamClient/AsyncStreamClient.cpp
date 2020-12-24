d2336f928815bc4572b3c7f740b7bb7d0b64f0db
taeyeong201 <dkwkrh201@naver.com>
2020-12-24 22:48:33 +0900

    AsyncStream 개발 셋팅

---
+++ GRPCSimpleTest/AsyncStreamClient/AsyncStreamClient.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+#include <grpcpp/grpcpp.h>
+
+#include "stream.grpc.pb.h"
+
+
+using namespace grpc;
+using namespace simpleStream;
+
+
+int main() {
+	return 0;
+}
---
+++ GRPCSimpleTest/AsyncStreamServer/AsyncStreamServer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+#include <grpcpp/grpcpp.h>
+
+#include "stream.grpc.pb.h"
+
+using namespace grpc;
+using namespace simpleStream;
+
+
+int main() {
+	
+
+	return 0;
+}
